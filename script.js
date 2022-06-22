aa069df16055c228d86e62b1d344a7efb5b26fa4
djtravz <djtravz@gmail.com>
2022-06-22 19:12:07 -0400

    minor things

--- script.js
+++ script.js
@@ -50,4 +50,15 @@ function show (elements) {
     }
 }
 
+$(window).scroll(function(e){ 
+    var $el = $('.fixedElement'); 
+    var isPositionFixed = ($el.css('position') == 'fixed');
+    if ($(this).scrollTop() > 200 && !isPositionFixed){ 
+      $el.css({'position': 'fixed', 'top': '0px'}); 
+    }
+    if ($(this).scrollTop() < 200 && isPositionFixed){
+      $el.css({'position': 'static', 'top': '0px'}); 
+    } 
+  });
+
 show("intro")
